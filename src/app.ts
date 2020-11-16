a0ee12942c64c2d1812f34fbcb91ccf62b816c97
Totoratsu <mayor.daniel.18@gmail.com>
2020-11-15 19:34:54 -0500

    TS Template

---
+++ src/app.ts
@@ -0,0 +1,58 @@
+import express, { Application } from 'express';
+import mongoose from 'mongoose';
+import passport from 'passport';
+import morgan from 'morgan';
+import cors from 'cors';
+import { config } from 'dotenv';
+
+/* import IndexRoutes from './routes/index.routes';
+import UserRoutes from './routes/user.routes';
+import { jwtUser, seed } from './middlewares/user.middlewares';
+import userModel from './models/user.model'; */
+
+/* Initializations */
+const app: Application = express();
+if (process.env.NODE_ENV === 'dev' || process.env.NODE_ENV === undefined)
+    config();
+
+//Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+/* passport.use('userJwt', jwtUser); */
+app.use(morgan('dev'));
+app.use(cors());
+
+/* passport.serializeUser((user: any, done) => {
+    done(null, user._id);
+});
+
+passport.deserializeUser(async (id: any, done) => {
+    const user = await userModel.findById(id);
+    if (!user)
+        return done(new Error('UserNotFound'), false);
+
+    return done(null, user);
+}); */
+
+// Routes
+/* app.use('/', IndexRoutes);
+app.use('/user', UserRoutes); */
+
+/* DB and server setup */
+mongoose.connect(
+    process.env.MONGO_URI || 'mongodb://localhost:27017/api-crud-cm',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    }, (err) => {
+        if (err) throw err;
+        console.log('DB connected');
+    }
+);
+app.listen(process.env.PORT || 65000, () => {
+    console.log(`Server running in port ${process.env.PORT || 65000}`);
+});
+console.log(`Server running in mode ${process.env.NODE_ENV || 'dev'}`);
